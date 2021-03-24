499fe2e1759aa6b7f41669e546bbbe339ff33a72
Pierre <pierre.crbs@gmail.com>
2021-03-24 16:47:41 +0100

    add Timeout

--- app/src/main/java/com/pierre/yugiohkotlinapp/cards/RetrofitInstance.kt
+++ app/src/main/java/com/pierre/yugiohkotlinapp/cards/RetrofitInstance.kt
@@ -5,23 +5,27 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 class RetrofitInstance {
 	companion object {
 		private const val BASE_URL = "https://db.ygoprodeck.com/api/v7/"
 
-		private val interceptor : HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+		private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
 			this.level = HttpLoggingInterceptor.Level.BODY
 		}
-		private val client : OkHttpClient = OkHttpClient.Builder().apply {
+		private val client: OkHttpClient = OkHttpClient.Builder().apply {
 			this.addInterceptor(interceptor)
+				.connectTimeout(30, TimeUnit.SECONDS)
+				.readTimeout(20, TimeUnit.SECONDS)
+				.writeTimeout(25, TimeUnit.SECONDS)
 		}.build()
 
 		fun getRetrofitInstance(): Retrofit {
 			return Retrofit.Builder()
 				.baseUrl(BASE_URL)
-				.addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
 				.client(client)
+				.addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
 				.build()
 		}
 	}
