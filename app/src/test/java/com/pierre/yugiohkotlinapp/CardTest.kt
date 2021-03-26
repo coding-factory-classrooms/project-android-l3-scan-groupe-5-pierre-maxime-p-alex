aed4c387219960bd11442309674ed76949002c5a
Pierre <pierre.crbs@gmail.com>
2021-03-26 14:46:35 +0100

    add planned testing

--- app/src/test/java/com/pierre/yugiohkotlinapp/CardTest.kt
+++ app/src/test/java/com/pierre/yugiohkotlinapp/CardTest.kt
@@ -1,9 +1,22 @@
 package com.pierre.yugiohkotlinapp
 
-import org.junit.Assert
 import org.junit.Test
 
 class CardTest {
+
+	// Planned testing :
+
+	// 1) Check that the card ID entered by the user contains 8 digits
+
+	// 2) Check if the send Card was correctly remove
+
+	// 3) Check if card already exist in the database
+
+	// 4) Test the API by send a POST request to BASE_URL/cardinfo.php?&id=89631139
+
+	// 5) Check if camera is granted
+
+	// 6) Check if Database successfully start
 
 	@Test
 	fun `invalid cardId return error`() {
